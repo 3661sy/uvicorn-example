7bdf0f99e1dc5c0af1aafc4497aeef817e714c4d
Parca <flsk3661@gmail.com>
2022-05-26 10:03:01 +0900

    [Add] request

--- request.py
+++ request.py
@@ -1,3 +1,7 @@
 class Request:
     def __init__(self, scope, receive, send) -> None:
-        pass
+        self.scope = scope
+    
+    @property
+    def method(self):
+        return self.scope['method']
